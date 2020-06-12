e4a030252ae97dbf608a47af6446b53b612ea308
TashaKim-MJ <mijookim.tasha@gmail.com>
2020-06-13 02:33:34 +0900

    Added code to process xml data

--- php_project/ViewController.swift
+++ php_project/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let key = "gm%2BdwQ8JPRsI4UOY0H2GnpG5dFW8G6RuFiPYQ0GGSFW26pySBSKHAvUhwthyUzEdmzjH6qHEQ2vuE%2FbX5RgYMQ%3D%3D"
+        let url = "http://apis.data.go.kr/6260000/MedicInstitService&serviceKey=\(key)"
+        
+        let xmlParser = XMLParser(contentsOf: URL(string: url)!)
+        xmlParser!.delegate = self
+        xmlParser!.parse()
     }
-
-
+    
+      
 }
 
