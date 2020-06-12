ae7281e8c2b979ae71925439dc192fd63c9db77c
TashaKim-MJ <mijookim.tasha@gmail.com>
2020-06-13 07:20:31 +0900

    add structure for JSON data

--- php_project/ViewController.swift
+++ php_project/ViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftyXMLParser
 
 class ViewController: UIViewController {
 
-    let key = "gm%2BdwQ8JPRsI4UOY0H2GnpG5dFW8G6RuFiPYQ0GGSFW26pySBSKHAvUhwthyUzEdmzjH6qHEQ2vuE%2FbX5RgYMQ%3D%3D"
-    let hospitalurl = "http://apis.data.go.kr/6260000/MedicInstitService&serviceKey"
+    struct hospital: Codable {
+        var name: String        // 병원 이름
+        var addr: String        // 주소
+        var x: Double           // x 좌표
+        var y: Double           // y 좌표
+        var tel: String         // 전화번호
+        var medinst: String     // 의료기관종별명 ex.요양병원, 한방병원
+        var sbjects: [String]      // 진료과목내용 ex. 내과, 가정의학과, 한방내과
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
