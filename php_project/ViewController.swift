bbcbd3db88fa3594a1533b498d621e033ff8b143
Smineee11 <smin3407ch@gmail.com>
2020-06-14 19:40:17 +0900

    Show date in console

--- php_project/ViewController.swift
+++ php_project/ViewController.swift
@@ -12,9 +12,7 @@ import RealmSwift
 
 class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
     
-    
-
-    let encoder = JSONEncoder() 
+    let encoder = JSONEncoder()
     
     struct hospital: Codable {
         var name: String        // 병원 이름
@@ -29,15 +27,17 @@ class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate
     @IBOutlet weak var outputtext: UILabel!
     @IBOutlet var calendar: FSCalendar!
     
-    let realm = try! Realm()
+  
     var memo : Results<HealthMemo>?
     
     @IBAction func insert(_ sender: Any) {
         var textField = UITextField()
+        
         let alert = UIAlertController(title: "오늘의 건강을 기록하세요", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "추가하기", style: .default) { (action) in
             let new = HealthMemo()
             new.write = textField.text!
+            
             self.save(healthMemo: new)
         }
         alert.addTextField{
@@ -50,21 +50,19 @@ class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate
         
     }
     
+    let realm = try! Realm(configuration: Realm.Configuration(schemaVersion: 2))
+
+    
     func save(healthMemo: HealthMemo){
         print("save")
         do{
             try realm.write{
                 realm.add(healthMemo)
                
-
-                    let result =  realm.objects(HealthMemo.self)
-
-                       for item in result {
-
-                          print("name= \(item.write)")
-
-
-                       }
+                let result =  realm.objects(HealthMemo.self)
+                for item in result {
+                    print("name= \(item.write)")
+                }
 
             }
         }catch{
@@ -72,17 +70,44 @@ class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate
         }
         //self.outputtext.reloadData()
     }
-
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar?.allowsMultipleSelection = false
+        calendar?.delegate = self
+        calendar?.dataSource = self
         // Do any additional setup after loading the view.
-        
     }
+    
+
+   // 날짜 선택 시 콜백 메소드
+      public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+          let dateFormatter = DateFormatter()
+           dateFormatter.dateFormat = "YYYY-MM-dd"
+         print(dateFormatter.string(from: date))
+        
+
+       }
+    
+       // 날짜 선택 해제 시 콜백 메소드
+       public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+           let dateFormatter = DateFormatter()
+           dateFormatter.dateFormat = "YYYY-MM-dd"
+           print(dateFormatter.string(from: date))
+       }
+
+    
+    
     
 }
 
+
+class HealthMemo: Object {
+   
+    @objc dynamic var year:String = ""
+    @objc dynamic var month:String = ""
+    @objc dynamic var day:String = ""
+    @objc dynamic var write: String = ""
+}
 
 
