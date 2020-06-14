d66627c92e0e2ccc14049efb8025b890fa66683b
minju-Kang <kelly991022@naver.com>
2020-06-14 18:14:16 +0900

    hospital class

---
+++ php_project/Hospital.swift
@@ -0,0 +1,21 @@
+//
+//  Hospital.swift
+//  php_project
+//
+//  Created by minjuKang on 2020/06/14.
+//  Copyright © 2020 Mijoo Kim. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Hospital: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var address = ""
+    @objc dynamic var xlocation: Double = 0
+    @objc dynamic var ylocation: Double = 0
+    @objc dynamic var telephone = ""
+    @objc dynamic var medinst = ""
+    @objc dynamic var subject = ""
+    dynamic var reviews = [String]()
+}
