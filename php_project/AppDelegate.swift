69d395f8dbc5f36e9bccd0549064b10d00bfa492
sujineel <lsj990611@gmail.com>
2020-06-16 21:06:07 +0900

    map view modify

--- php_project/AppDelegate.swift
+++ php_project/AppDelegate.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    var locationManager: CLLocationManager?
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
         return true
     }
 
@@ -30,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+     
     }
 
 
--- php_project/mapController.swift
+++ php_project/mapController.swift
@@ -8,67 +8,102 @@
 
 import UIKit
 import MapKit
-class mapController: UIViewController, CLLocationManagerDelegate {
+import CoreLocation
 
-    @IBOutlet var myMap: MKMapView!
-    @IBOutlet var lblLocationinfo1: UILabel!
-    @IBOutlet var lblLocationinfo2: UILabel!
+class mapController: UIViewController, MKMapViewDelegate {
+    
+
+    @IBOutlet var mapView: MKMapView!
+    
+ 
+    
+    @IBAction func zoomIn(_ sender: Any) {
+    
+    let userLocation = mapView.userLocation//현재 위치 좌표, 남/북 2000미터 스팬
+        
+    let region = MKCoordinateRegion(center: userLocation.location!.coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.setRegion(region, animated: true)
+    }
+
   
-    let locationManager = CLLocationManager()
+    @IBAction func changeMapType(_ sender: Any) {
     
+
+        if mapView.mapType == MKMapType.standard {
+            mapView.mapType = MKMapType.satellite
+        }
+        else {
+            mapView.mapType = MKMapType.standard
+        }
+        
+    }
+   
     
+ 
+    @IBOutlet var searchText: UITextField!
+    
+   
+    var matchingItems: [MKMapItem] = [MKMapItem]()
+    
+    @IBAction func textFieldReturn(_ sender: Any)
+    {
+        resignFirstResponder()
+        mapView.removeAnnotation(mapView.annotations as! MKAnnotation)
+        self.performSearch()
+       
+    }
+    
+    func performSearch()  {
+         // 배열 값 삭제
+        matchingItems.removeAll()
+        let request = MKLocalSearch.Request()
+        // 텍스트 필드의 값으로 초기화된 MKLocalSearchRequest 인스턴스를 생성
+        request.naturalLanguageQuery = searchText.text
+        request.region = mapView.region
+        // 검색 요청 인스턴스에 대한 참조체로 초기화
+        let search = MKLocalSearch(request: request)
+        // MKLocalSearchCompletionHandler 메서드가 호출되면서 검색이 시작
+        search.start(completionHandler: {(response: MKLocalSearch.Response!, error: Error!) in
+                   if error != nil {
+                       print("Error occured in search: \(error.localizedDescription)")
+                   } else if response.mapItems.count == 0 {
+                       print("No matches found")
+                   } else {
+                       print("Matches found")
+                       // 일치된 값이 있다면 일치된 위치에 대한 mapItem 인스턴스의 배열을 가지고 mapItem 속성에 접근한다.
+                       for item in response.mapItems as [MKMapItem] {
+                           if item.name != nil {
+                               print("Name = \(item.name!)")
+                           }
+                           if item.phoneNumber != nil {
+                               print("Phone = \(item.phoneNumber!)")
+                           }
+                           
+                           self.matchingItems.append(item as MKMapItem)
+                           print("Matching items = \(self.matchingItems.count)")
+                           // 맵에 표시할 어노테이션 생성
+                           let annotation = MKPointAnnotation()
+                           // 위치에 어노테이션을 표시
+                           annotation.coordinate = item.placemark.coordinate
+                           annotation.title = item.name
+                           annotation.subtitle = item.phoneNumber
+                           self.mapView.addAnnotation(annotation)
+                       }
+                   }
+               })
+           }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblLocationinfo1.text = ""
-        lblLocationinfo2.text = ""
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        myMap.showsUserLocation = true
+        mapView.showsUserLocation = true
+        mapView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-    func goLocation(latitudeValue : CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double ){
-        let pLocaiton = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
-        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
-        let pRegion = MKCoordinateRegion(center: pLocaiton, span: spanValue)
-        myMap.setRegion(pRegion, animated: true)
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation){
+        mapView.centerCoordinate = userLocation.location!.coordinate
     }
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let pLocation  = locations.last
-        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
-        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {(placemarks, error) -> Void in
-            let pm = placemarks!.first
-            let country  = pm!.country
-            var address:String = country!
-            if pm!.locality != nil {
-                address += " "
-                address += pm!.locality!
-            }
-            if pm!.thoroughfare != nil {
-                        address += " "
-                        address += pm!.thoroughfare!
-            }
-            self.lblLocationinfo1.text = "현재 위치"
-            self.lblLocationinfo2.text = address
-            
-            
-        })
-        locationManager.stopUpdatingLocation()
-    }
-    
-    
-    @IBOutlet var sgChangeLocation: UISegmentedControl!
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
